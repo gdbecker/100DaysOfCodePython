a73520e8ecd035b07c43287b6dd943c0d59c39f1
Garrett Becker <garrettdbecker@gmail.com>
2023-06-09 13:07:40 -0400

    Updates

--- showcase/app/page.js
+++ showcase/app/page.js
@@ -12,6 +12,30 @@ import ProjectCard from './components/ProjectCard';
         type="scripting"
       />
 
+        <ProjectCard 
+          img_bg="bg-space-missions"
+          title="Space Missions Analysis"
+          demo_url="https://www.kaggle.com/embed/garrettbecker/space-missions-analysis?kernelSessionId=132508838"
+          code_url=""
+          type="dataScience"
+        />
+
+        <ProjectCard 
+          img_bg="bg-fatal-force"
+          title="Fatal Force Analysis"
+          demo_url="https://www.kaggle.com/embed/garrettbecker/fatal-force-analysis?kernelSessionId=132688822"
+          code_url=""
+          type="dataScience"
+        />
+
+        <ProjectCard 
+          img_bg="bg-determinants-earnings"
+          title="Determinants of Earnings Analysis"
+          demo_url="https://www.kaggle.com/embed/garrettbecker/determinants-of-earnings-analysis?kernelSessionId=132695898"
+          code_url=""
+          type="dataScience"
+        />
+
     </main>
   )
 }
