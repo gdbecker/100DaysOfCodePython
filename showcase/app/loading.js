a70d7633568ed722415a25543b2fded5d2cd1220
Garrett Becker <garrettdbecker@gmail.com>
2023-07-31 19:19:26 -0400

    Create loading.js

---
+++ showcase/app/loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ function LoadingPage() {
+  
+  return (
+    <main className="flex flex-col w-full min-h-screen p-10 justify-start bg-lightGray 2xl:px-36">
+      <h1 className="pt-10 font-sourceSansProBold text-center text-grayishBlue text-xl">LOADING</h1>
+    </main>
+  )
+
+}
+
+export default LoadingPage;
