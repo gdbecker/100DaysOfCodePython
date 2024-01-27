b476c2a840b816260024ed851d2ef72c4a2ab5a3
Garrett Becker <garrettdbecker@gmail.com>
2024-01-27 17:03:57 -0500

    Update Footer.js

--- showcase/app/components/Footer.js
+++ showcase/app/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="h-fit p-5 font-sourceSansProRegular text-center text-black text-sm border-grayishBlue border-t-2 bg-lightGray 2xl:px-36">
       Course from <a href="https://www.udemy.com/course/100-days-of-code/" target="_blank" className="text-blue hover:text-yellow">Udemy</a>. 
-      Designed and developed with ❤️ by <a href="https://github.com/gdbecker/100DaysOfCodePython/tree/main" target="_blank" className="text-blue hover:text-yellow">Garrett Becker</a>. &copy; 2023-2024 All rights reserved.
+      Designed and developed with ❤️ by <a href="https://github.com/gdbecker/100DaysOfCodePython/tree/main" target="_blank" className="text-blue hover:text-yellow">Garrett Becker</a>. &copy; 2023-{new Date().getFullYear()} All rights reserved.
     </div>
   )
 }
